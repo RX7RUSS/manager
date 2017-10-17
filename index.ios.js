dc0ee5659c23d9b5f8bd52d79cb18e644a3fec6a
RX7RUSS <russ.savage5@gmail.com>
2017-10-17 11:32:48 -0500

    Changes to Both Index Files

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App.js';
 
-AppRegistry.registerComponent('ios-resume', () => App);
+AppRegistry.registerComponent('manager', () => App);
