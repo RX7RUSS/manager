1d06b3dfb1868998a868129dfef053b293cf26d7
RX7RUSS <russ.savage5@gmail.com>
2017-11-01 20:40:20 -0500

    Firebase Imported And Initialized

--- App.js
+++ App.js
@@ -3,9 +3,22 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { StyleSheet, Text, View } from 'react-native';
 import reducers from './src/reducers';
+import firebase from 'firebase';
 
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDTbesnS9_gTVsGBWVBe1fz-hDgvxPU6k8',
+      authDomain: 'manage-b486e.firebaseapp.com',
+      databaseURL: 'https://manage-b486e.firebaseio.com',
+      projectId: 'manage-b486e',
+      storageBucket: 'manage-b486e.appspot.com',
+      messagingSenderId: '933343990113'
+    };
+  firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
