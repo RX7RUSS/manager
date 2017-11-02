8e9f4fc756f556c59641fe51d447cd7d53254e20
RX7RUSS <russ.savage5@gmail.com>
2017-11-01 20:28:58 -0500

    Fixed Reducer Error

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { StyleSheet, Text, View } from 'react-native';
-import { reducers } from './src/reducer';
+import reducers from './reducers';
 
-const store = createStore(reducers)
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
+      <Provider store={createStore(reducers)}>
         <View style={styles.container}>
           <Text>Redux Manager Scheduling App</Text>
         </View>
