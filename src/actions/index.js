29d6dbe33a156b33fb02006dc3602c716bf572d1
RX7RUSS <russ.savage5@gmail.com>
2017-11-14 20:22:45 -0600

    Password Action Added

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,10 @@ export const emailChanged = (text) => {
     payload: text
   };
 })
+
+export const passwordChanged = (text) => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: text
+  };
+})
