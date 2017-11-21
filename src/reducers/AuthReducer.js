463a9c1475a49e47419077b7cb2d18377551cb2e
RX7RUSS <russ.savage5@gmail.com>
2017-11-20 21:19:49 -0600

    AuthReducer Change

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -5,7 +5,9 @@ const INITITAL_STATE = { email: '' };
 export default (state = INITITAL_STATE, action) => {
   switch (action.type) {
     case: EMAIL_CHANGED
-
+    return: state.emailChanged,
+    case: PASSWORD_CHANGED
+    return: state.passwordChanged,
     default:
       return state;
   }
