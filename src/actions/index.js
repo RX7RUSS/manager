9e283a32106d45a20c7a841598187fb09c962dc0
RX7RUSS <russ.savage5@gmail.com>
2017-11-21 21:59:02 -0600

    Redux-Thunk Wired Up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
+import firebase from 'firebase';
 import { EMAIL_CHANGED } from './types';
-import { PASSWORD_CHANGED } from './types';
+import { PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -13,4 +14,27 @@ export const passwordChanged = (text) => {
     type: PASSWORD_CHANGED,
     payload: text
   };
+};
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(user => loginUserSuccess(dispatch, user))
+        .catch(() => LoginUserFail(dispatch));
+    });
+  };
+};
+
+const LoginUserFail = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAIL });
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
 };
