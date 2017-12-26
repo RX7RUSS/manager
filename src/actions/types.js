e586d85c8e90c32d6abb352cb608e6526df2d021
RX7RUSS <russ.savage5@gmail.com>
2017-12-25 23:15:12 -0600

    Types

--- src/actions/types.js
+++ src/actions/types.js
@@ -6,6 +6,3 @@ export const LOGIN_USER_FAIL = 'login_user_fail';
 export const LOGIN_USER = 'login_user';
 export const ADD_EMPLOYEE = 'add_employee';
 export const FIRE_EMPLOYEE = 'fire_employee';
-export const SEND_TEXT = 'send_text';
-export const CHANGE_SHIFT = 'change_shift';
-export const SWAP_SHIFT = 'swap_shift';
